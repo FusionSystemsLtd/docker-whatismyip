37984f27a20aafd045f0fee8aef16387599560cc
Mark Rains <46301052+FusionSystemsLtd@users.noreply.github.com>
2019-01-01 18:18:58 +0000

    Update index.php
    Started to split out the important sections of the SERVER var.

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<h3>REMOTE ADDRESS: <?php echo $_SERVER['REMOTE_ADDR'] ?></h3><br/>
+<h3>SERVER ADDRESS: <?php echo $_SERVER['SERVER_ADDR'] ?></h3><br/>
+<hr />
 <h1>VAR DUMP OF SERVER</h1>
 <hr>
 <?php
